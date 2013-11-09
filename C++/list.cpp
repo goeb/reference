9b55b13cc7adaf281a02103074ef60358c9885b4
fred <fred@fred1>
2013-11-09 20:57:27 +0100

    modify items inplace in list

--- C++/list.cpp
+++ C++/list.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-using namespace std;
 #include <list>
 
 main() {
-	list<string> L = { "toto", "zzz" }; // xxx wrong !!
+    std::list<std::string> L;
+    L.push_back("toto");
+    L.push_back("titi");
+    L.push_back("tutu");
+    std::list<std::string>::iterator i;
+    for (i=L.begin(); i!=L.end(); i++) {
+        std::cout << *i << ", ";
+    }
+    std::cout << "\n";
+
+    for (i=L.begin(); i!=L.end(); i++) {
+        *i = (*i) + "-xx";
+    }
+
+    for (i=L.begin(); i!=L.end(); i++) {
+        std::cout << *i << ", ";
+    }
+    std::cout << "\n";
 }
