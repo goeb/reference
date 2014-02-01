bb6fc291f707e63eb22c64702772244bed77e289
fred <fred@fred1>
2014-02-01 13:35:10 +0100

    more about sscanf

--- C/sscanf.c
+++ C/sscanf.c
@@ -7,4 +7,11 @@ main()
     char *sectionName = 0;
     int n = sscanf(s, "[%sa]", sectionName);
     printf("n=%d, sectionName=%s\n", n, sectionName);
+
+    s = "Content-Disposition: form-data; name=\"file\"; filename=\"accum.png\"";
+    char x[1024];
+    char y[1024];
+    char fname[1014];
+    n = sscanf(s, "Content-Disposition: %*s %*s filename=\"%1023[^\"]", x, y, fname);
+    printf("n=%d, x=%s, y=%s, fname=%s\n", x, y, fname);
 }
