a15424447729c73235dae15a24242871988b5bac
Frederic Hoerni <fhoerni@free.fr>
2015-01-05 12:54:52 +0100

    tested ok

--- C/ldapClient.c
+++ C/ldapClient.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <ldap.h>
 
+void usage()
+{
+	printf("usage: ldapClient <server> <CN> <password>\n");
+	printf("\n");
+	printf("Example:");
+	printf("    ldapClient ldap://example.com:389 \"John Doo\" abcdef\n");
+}
+
 int main(int argc, char **argv)
 {
     LDAP        *ld;
@@ -10,13 +18,14 @@ int main(int argc, char **argv)
     /* Get username and password */
     if( argc != 4 )
     {
-        perror( "invalid args, required: server who password" );
+        perror( "invalid args, required: <server> <CN> <password>" );
+		usage();
         return( 1 );
     }
     const char *server = argv[1]; // ex: "ldap://nafiux.com:389"
-    const char *who = argv[2]; // ex: cn=toto,ou=People,dc=nafiux,dc=com
+    const char *cn = argv[2]; // common name
     const char *password = argv[3];
-    printf( "Connecting as %s...\n", who );
+    printf( "Connecting as %s...\n", cn );
 
     /* Open LDAP Connection */
     if( ldap_initialize( &ld, server ) )
@@ -26,7 +35,7 @@ int main(int argc, char **argv)
     }
 
     /* User authentication (bind) */
-    rc = ldap_simple_bind_s( ld, who, password);
+    rc = ldap_simple_bind_s( ld, cn, password);
     if( rc != LDAP_SUCCESS )
     {
         fprintf(stderr, "ldap_simple_bind_s: %s\n", ldap_err2string(rc) );
