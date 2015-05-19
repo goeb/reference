e86c459c14610c167f3c7ee9483429d5d2afe57e
Frederic Hoerni <fhoerni@free.fr>
2015-05-19 15:09:51 +0200

    test open file descriptors through 'system'

--- C/system.c
+++ C/system.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 
 int main()
 {
-	system("ls tytyty");
+	FILE *f1 = fopen("system.c", "r");
+	FILE *f2 = fopen("system.c", "r");
+	system("ls -al /proc/self/fd");
 }
