6f37923d66916dcf7e99e9c684ad89ff4796b18d
Frederic Hoerni <fhoerni@free.fr>
2015-07-09 12:11:53 +0200

    improved mqueue demo

--- C/mqueue_master.c
+++ C/mqueue_master.c
@@ -1,7 +1,12 @@
-       #include <fcntl.h>           /* For O_* constants */
-       #include <sys/stat.h>        /* For mode constants */
-       #include <mqueue.h>
+#include <stdlib.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define MQ_MSG_SIZE 100
 
 main()
 {
@@ -9,28 +14,59 @@ main()
 
 	struct mq_attr attr;  
 	attr.mq_flags = 0;  
-	attr.mq_maxmsg = 10;  
-	attr.mq_msgsize = 33;  
+	attr.mq_maxmsg = 20;  
+	attr.mq_msgsize = MQ_MSG_SIZE;  
 	attr.mq_curmsgs = 0;  
 	
 	// Create queue with mq_open in master process (doc):
 	
-	mqd_t queue = mq_open(qname, O_CREAT|O_RDWR, 0644, &attr);
+	mqd_t queue = mq_open(qname, O_CREAT|O_RDWR|O_NONBLOCK, 0644, &attr);
 
 	if (-1 == queue) perror("mq_open: ");
 	//queue = mq_open(qname, O_RDONLY);
 	
 	// In reader process open queue for reading:
-	char rcvmsg[50+1];
+	char rcvmsg[MQ_MSG_SIZE];
 
 	while (1) {
-		int iret = mq_receive(queue, rcvmsg, 50, 0);
+		struct mq_attr attr;
+		int r = mq_getattr(queue, &attr);
+		printf("Pending messages in queue: %ld\n", attr.mq_curmsgs);
+		// prepare 'select'
+		fd_set rd;
+		int nfds = -1;
+		FD_ZERO(&rd);
+		FD_SET(queue, &rd); // file descriptor
+		nfds = queue + 1;
 
-		rcvmsg[50] = 0;
-		if (-1 == iret) {
-			perror("master error: ");
-		} else printf("master got: %s\n", rcvmsg);
-		sleep(1);
+		int status = select(nfds+1, &rd, 0, 0, 0);
+		if (status == 0) {
+			printf("select returned: %d\n", status);
+		} else if (status < 0) {
+			if (errno == EINTR) {
+				printf("select got: EINTR (interrupted by a signal)\n");
+			} else {
+				printf("select error: %s\n", strerror(errno));
+				exit(1);
+			}
+		} else {
+			if (FD_ISSET(queue, &rd)) {
+				int iret = mq_receive(queue, rcvmsg, MQ_MSG_SIZE, 0);
+				if (-1 == iret) {
+					printf("mq_receive error: %s\n", strerror(errno));
+				} else {
+					printf("master got: [%s]\n", rcvmsg);
+					if (0 == strcmp(rcvmsg, "quit")) {
+						int r = mq_unlink(qname);
+						printf("mq_unlink: %d, %s\n", r, strerror(errno));
+						exit(0);
+					}
+				}
+				
+			} else {
+				printf("select indication, but queue not ready\n");
+			}
+		}
 	}
 	
 
--- C/mqueue_slave.c
+++ C/mqueue_slave.c
@@ -1,23 +1,25 @@
+#include <errno.h>
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <mqueue.h>
 #include <stdio.h>
 #include <string.h>
 
-main()
+main(int argc, char **argv)
 {
+	const char *msg = "hello";
+	if (argc > 1) msg = argv[1];
+
 	const char * qname = "/xxx";
 
-	struct mq_attr attr;  
-	attr.mq_flags = 0;  
-	attr.mq_maxmsg = 10;  
-	attr.mq_msgsize = 33;  
-	attr.mq_curmsgs = 0;  
-	
 	// open queue for writing:
-	mqd_t queue = mq_open(qname, O_WRONLY);
-	char buf[100];
-	sprintf(buf, "some message from=%d", getpid());
-	mq_send(queue, buf, strlen(buf)+1, 1);
+	mqd_t queue = mq_open(qname, O_WRONLY|O_NONBLOCK);
+
+	struct mq_attr attr;
+    int r = mq_getattr(queue, &attr);
+    printf("Info on queue: maxmsg=%ld, msgsize=%ld, curmsgs=%ld\n", attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+
+	r = mq_send(queue, msg, strlen(msg)+1, 1);
+	if (r != 0) printf("mq_send error: %s\n", strerror(errno));
 	
 }
