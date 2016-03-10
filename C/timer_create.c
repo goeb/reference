17969c61ca54d9b46f5066217f0aa6fef2b1d127
Frederic Hoerni <fhoerni@free.fr>
2016-03-10 10:15:14 +0100

    test timer_create

--- C/timer_create.c
+++ C/timer_create.c
@@ -6,6 +6,60 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+
+pthread_t t1;
+
+void * thread1(void *p)
+{
+    printf("thread1: starting...\n");
+	sigset_t signal_set;
+	int sig;
+
+	while(1) {
+		/* wait for any and all signals */
+		sigfillset( &signal_set );
+		sigwait( &signal_set, &sig );
+		printf("thread1: got sig %d\n", sig);
+	}
+
+    printf("thread1: ending...\n");
+}
+
+void block_signals() {
+	int err;
+	sigset_t sigmask;
+
+	/* Block all signals so that we handle them in a dedicated thread */
+	sigfillset(&sigmask);
+	err = pthread_sigmask(SIG_BLOCK, &sigmask, NULL);
+	if (err) {
+		printf("pthread_sigmask error: %s", err, strerror(errno));
+	}
+}
+void launch_thread() {
+
+	block_signals();
+    pthread_attr_t attr;
+    pthread_create(&t1, NULL, &thread1, 0);
+}
+
+void stop_thread()
+{
+	int err = pthread_cancel(t1);
+	if (err) {
+		printf("pthread_cancel error: %s\n", strerror(err));
+		return;
+	}
+
+	/* Wait thread termination */
+	err = pthread_join(t1, NULL);
+	if (err) {
+		printf("pthread_join error: %s\n", strerror(err));
+		return;
+	}
+
+}
 
 
 void f(sigval_t value)
@@ -24,13 +78,21 @@ int main()
 	timer_t timer;
 
 	timer_create(CLOCK_MONOTONIC, &event, &timer);
+	
+	struct itimerspec timer_spec;
+	timer_spec.it_interval.tv_sec = 1;
+	timer_spec.it_interval.tv_nsec = 0;
+	timer_spec.it_value.tv_sec = 1;
+	timer_spec.it_value.tv_nsec = 0;
+	timer_settime(timer, 0, &timer_spec, NULL);
 
-	sleep(2);
+	launch_thread();
+	sleep(5);
+	stop_thread();
 
 	printf("timer_delete...\n");
 	timer_delete(timer);
 
-	sleep(2);
 	printf("exit 0...\n");
 	exit(0);
 }
