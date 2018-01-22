c85bec7f6d4eee51be93235e3b5caa3cc77f9f29
Frederic Hoerni <fhoerni@free.fr>
2018-01-22 20:59:10 +0100

    realpath.c

---
+++ C/realpath.c
@@ -0,0 +1,22 @@
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <file>\n", argv[0]);
+		return 1;
+	}
+
+	char resolved_path[PATH_MAX];
+	char *ptr = realpath(argv[1], resolved_path);
+	if (!ptr) {
+		fprintf(stderr, "realpath error: %s\n", strerror(errno));
+		return 1;
+	}
+
+	printf("%s\n", ptr);
+}
