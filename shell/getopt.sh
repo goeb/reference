a7e9e1008f5c3114ac6270ffd68f1eb00cdc0599
Frederic Hoerni <fhoerni@free.fr>
2018-03-26 21:51:41 +0200

    getopt.sh

---
+++ shell/getopt.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+usage() {
+	echo "usage: $0 [options] FILE ..."
+	echo
+	echo "Options:"
+	echo "    -v, --verbose"
+ 	echo "                    Be verbose"
+	echo
+	echo "    -o, --output FILE"
+	echo "                    Specify an output file name"
+	exit 1
+}
+
+TEMP=$(getopt -o vo: --long verbose,output: -n "$0" -- "$@")
+
+if [ $? != 0 ]; then exit 1; fi
+
+eval set -- "$TEMP"
+
+printArgs() {
+	while [ $# -ne 0 ]; do
+		echo printArgs: arg=$1
+		shift
+	done
+}
+
+printArgs "$@"
+
+# extract options
+while [ $# -ne 0 ]; do
+	case "$1" in
+		-o|--output) output=$2; shift 2;;
+		-v|--verbose) verbose=1; shitf;;
+		--) shift; break;;
+		*) usage;;
+	esac
+done
+
+echo verbose=$verbose
+echo output=$output
+
+# non-option arguments
+while [ $# -ne 0 ]; do
+	echo arg=$1
+	shift
+done
+
