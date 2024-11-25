2b40eb337c97d5375ed3a73575b2e34f2a01a49e
Frederic Hoerni <fhoerni@free.fr>
2024-11-25 21:47:26 +0100

    go: synchro goroutines with a channel

--- go/test_goroutine.go
+++ go/test_goroutine.go
@@ -10,33 +10,38 @@ import (
 
 var f_count, g_count int
 
-func f() {
+func f(ch chan string) {
 	for i := 0; i < 5; i++ {
 		fmt.Println("f: f_count=", f_count)
 		f_count++
 		time.Sleep(100 * time.Millisecond)
+		ch <- "hello from f"
 	}
 }
 
-func g() {
+func g(ch chan string) {
 	for i := 0; i < 5; i++ {
 		fmt.Println("g: g_count=", g_count)
 		g_count++
+		msg := <-ch
+		fmt.Println("g: received: ", msg)
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
 func main() {
+	messages := make(chan string)
+
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		f()
+		f(messages)
 	}()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		g()
+		g(messages)
 	}()
 
 	wg.Wait()
