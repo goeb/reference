3aeb3044bb3e31cba7a6afe378ce8759488d3993
Frederic Hoerni <fhoerni@free.fr>
2025-03-06 21:53:30 +0100

    inheritor.cpp: more tests

--- C++/inheritor.cpp
+++ C++/inheritor.cpp
@@ -1,30 +1,36 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 class A {
     public:
-    A() {
-        cout << "A()" << endl;
-    }
-    ~A() {
-        cout << "~A()" << endl;
-    }
+    A()  { printf("A()\n"); }
+    ~A() { printf("~A()\n"); }
+	A& operator=(const A&) { printf("A=()\n"); return *this; }
 };
 
-class B : public A
-{
+class B : public A {
     public:
-    B() {
-        cout << "B()" << endl;
-    }
-    ~B() {
-        cout << "~B()" << endl;
-    }
+    B() { printf("B()\n"); }
+    ~B() { printf("~B()\n"); }
+	B& operator=(const B&) { printf("B=()\n"); return *this; }
 };
-main() {
-    A* a = new A();
-    delete a;
-    B* b = new B();
-    delete b;
+
+int main() {
+    A* a = new A(); // calls A()
+    delete a;       // calls ~A()
+    B* b = new B(); // calls A(), then B()
+    delete b;       // calls ~B() then ~A()
+	a = new B();    // calls A(), then B()
+	delete a;       // calls only ~A(), because ~A() is not virtual
+
+	// Test operator=
+	A a1, a2;
+	a1 = a2;         // calls A=()
+	B b1, b2;
+	b1 = b2;         // calls B=()
+	A* a3 = new B();
+	(*a3) = a2;      // calls A=()
+	delete a3;
+
+	return 0;
 }
 
